7346d74ea4efce1dcfbe3bdf2f6677d95b8b3b89
kuldeep230 <ramdeep214@gmail.com>
2021-05-12 04:33:45 +0530

    Converted User component from class to function component and also implemented UseEffect hook

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
 import Alert from "./Alert";
 import UserItem from "./UserItem";
@@ -7,112 +7,133 @@ import Search from "./Search";
 import Spinner from "../Layout/Spinner";
 
 // class Users extends Component {
-//   state = {
-//     users: [],
+//   constructor(props) {
+//     super(props);
+//     this.mySearchComponent = React.createRef();
+//   }
+//   componentDidMount() {
+//     this.props.getAllUsers();
+//   }
+
+//   handleOnClick = () => {
+//     if (this.mySearchComponent.current) {
+//       this.mySearchComponent.current.scrollIntoView({
+//         behavior: "smooth",
+//         block: "nearest",
+//       });
+//     }
 //   };
-
-// async componentDidMount() {
-//   try {
-//     let res = await axios.get("https://api.github.com/users");
-//     this.setState({ users: res.data });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// searchUsers = async () => {
-//   try {
-//     let { data } = await axios.get(
-//       `https://api.github.com/users?q=${this.state.text}`
-//     );
-//     this.setState({ users: data.items });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
 //   render() {
 //     return (
-//   <div>
-//     {/* <Search searchUsers={this.searchUsers} /> */}
-//     <div>
-//       {this.state.users.map((element) => (
-//         <UserItem
-//           login={element.login}
-//           avatar_url={element.avatar_url}
-//           key={element.id}
-//         />
-//       ))}
-//     </div>
-//   </div>
-// );
+//       <div>
+//         <div className="pb-16">
+//           <HeroSection />
+//         </div>
+//         <div
+//           className="py-6 px-10 bg-blue-200 bg-opacity-75 rounded-full my-10 flex flex-col items-center animate-bounce hover:bg-yellow-300 hover:bg-opacity-75 cursor-pointer "
+//           onClick={this.handleOnClick}
+//         >
+//           <button>
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               class="h-6 w-6"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               stroke="currentColor"
+//             >
+//               <path
+//                 stroke-linecap="round"
+//                 stroke-linejoin="round"
+//                 stroke-width="2"
+//                 d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
+//               />
+//             </svg>
+//           </button>
+//         </div>
+//         {/* <Alert /> */}
+//         <div ref={this.mySearchComponent}>
+//           <Search searchUsers={this.props.searchUsers} />
+//           {this.props.loading ? (
+//             <Spinner />
+//           ) : (
+//             <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-10 mt-10 rounded ">
+//               {this.props.users.map((element) => (
+//                 <UserItem
+//                   login={element.login}
+//                   avatar_url={element.avatar_url}
+//                   key={element.id}
+//                 />
+//               ))}
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     );
 //   }
 // }
 
-class Users extends Component {
-  constructor(props) {
-    super(props);
-    this.mySearchComponent = React.createRef();
-  }
-  componentDidMount() {
-    this.props.getAllUsers();
-  }
+/*Converted to function component...   */
+const Users = (props) => {
+  const mySearchComponent = React.createRef();
 
-  handleOnClick = () => {
-    if (this.mySearchComponent.current) {
-      this.mySearchComponent.current.scrollIntoView({
+  useEffect(() => {
+    props.getAllUsers();
+    //eslint-disable-next-line
+  }, []);
+
+  const handleOnClick = () => {
+    if (mySearchComponent.current) {
+      mySearchComponent.current.scrollIntoView({
         behavior: "smooth",
         block: "nearest",
       });
     }
   };
-  render() {
-    return (
-      <div>
-        <div className="pb-16">
-          <HeroSection />
-        </div>
-        <div
-          className="py-6 px-10 bg-blue-200 bg-opacity-75 rounded-full my-10 flex flex-col items-center animate-bounce hover:bg-yellow-300 hover:bg-opacity-75 cursor-pointer "
-          onClick={this.handleOnClick}
-        >
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
-              />
-            </svg>
-          </button>
-        </div>
-        {/* <Alert /> */}
-        <div ref={this.mySearchComponent}>
-          <Search searchUsers={this.props.searchUsers} />
-          {this.props.loading ? (
-            <Spinner />
-          ) : (
-            <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-10 mt-10 rounded ">
-              {this.props.users.map((element) => (
-                <UserItem
-                  login={element.login}
-                  avatar_url={element.avatar_url}
-                  key={element.id}
-                />
-              ))}
-            </div>
-          )}
-        </div>
+  return (
+    <div>
+      <div className="pb-16">
+        <HeroSection />
       </div>
-    );
-  }
-}
+      <div
+        className="py-6 px-10 bg-blue-200 bg-opacity-75 rounded-full my-10 flex flex-col items-center animate-bounce hover:bg-yellow-300 hover:bg-opacity-75 cursor-pointer "
+        onClick={handleOnClick}
+      >
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
+            />
+          </svg>
+        </button>
+      </div>
+      {/* <Alert /> */}
+      <div ref={mySearchComponent}>
+        <Search searchUsers={props.searchUsers} />
+        {props.loading ? (
+          <Spinner />
+        ) : (
+          <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-10 mt-10 rounded ">
+            {props.users.map((element) => (
+              <UserItem
+                login={element.login}
+                avatar_url={element.avatar_url}
+                key={element.id}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Users;
